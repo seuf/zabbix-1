0dcadb570510be592aa7881d44138a9bc8a665d5
Oleg Shepetyuk <oleg.shepetjuk@ask.fm>
2017-05-15 11:19:45 +0300

    added TriggersGetActive function to list current issues

--- trigger.go
+++ trigger.go
@@ -39,10 +39,57 @@ type HostIds []HostId
 
 type Triggers []Trigger
 
+// TriggersGet gets all triggers
 func (api *API) TriggersGet(params Params) (res Triggers, err error) {
 	if _, present := params["output"]; !present {
 		params["output"] = "extend"
 	}
+	response, err := api.CallWithError("trigger.get", params)
+	if err != nil {
+		return
+	}
+	res = make(Triggers, len(response.Result.([]interface{})))
+	for i, h := range response.Result.([]interface{}) {
+		h2 := h.(map[string]interface{})
+		reflector.MapToStruct(h2, &res[i], reflector.Strconv, "json")
+
+		if hosts, ok := h2["hosts"]; ok {
+			reflector.MapsToStructs2(hosts.([]interface{}), &res[i].Hosts, reflector.Strconv, "json")
+		}
+	}
+
+	return
+}
+
+// TriggerGetByID gets trigger by Id only if there is exactly 1 matching trigger.
+func (api *API) TriggerGetByID(id string) (res *Trigger, err error) {
+	triggers, err := api.TriggersGet(Params{"triggerids": id})
+	if err != nil {
+		return
+	}
+
+	if len(triggers) == 1 {
+		res = &triggers[0]
+	} else {
+		e := ExpectedOneResult(len(triggers))
+		err = &e
+	}
+	return
+}
+
+// TriggersGetActive gets all current issues like on zabbix dashboard
+func (api *API) TriggersGetActive(params Params) (res Triggers, err error) {
+	if _, present := params["output"]; !present {
+		params["output"] = "extend"
+	}
+	params["expandDescription"] = 1
+	params["active"] = 1
+	params["withLastEventUnacknowledged"] = 1
+	params["monitored"] = 1
+	params["only_true"] = 1
+	params["skipDependent"] = 1
+	params["selectHosts"] = "host"
+
 	response, err := api.CallWithError("trigger.get", params)
 	if err != nil {
 		return
